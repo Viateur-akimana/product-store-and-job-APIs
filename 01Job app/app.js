0711f879be722415e81a5d7e1c02b9f18ec026d0
Viateur <akimanaviateur94@gmail.com>
2024-01-03 21:09:15 +0200

    JOb app apis

--- 01Job app/app.js
+++ 01Job app/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const connectDB = require('../db/connect');
 const app = express();
+const dotenv = require('dotenv')
 app.get('/',(req,res)=>{
     res.send("home page")
 })
+
+
+app.use(dotenv).defaultConfiguration();
 const port = process.env.PORT || 8080;
 const start=async()=>{
     try {
