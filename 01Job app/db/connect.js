411ccad5969f8dcef5b4fdce27b8fbc856579dcd
Viateur <akimanaviateur94@gmail.com>
2024-01-03 21:07:20 +0200

    JOb app apis

--- 01Job app/db/connect.js
+++ 01Job app/db/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-const connectDB = ()=>{
-return new mongoose.connect('mongodb://atlas-sql-647820beabf8cc5c13133383-x7ftt.a.query.mongodb.net/freecodecamp?ssl=true&authSource=admin',{
+const connectDB = (url)=>{
+return new mongoose.connect(url,{
     useNewUrlParser:true,
     useUnifiedTopology:true,
     useCreateIndex:true,
