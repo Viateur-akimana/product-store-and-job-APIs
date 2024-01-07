86f7feeca6ff55457882d115c83c35daedd600ee
Viateur <akimanaviateur94@gmail.com>
2024-01-07 18:22:02 +0200

    job app apis

--- 01Job app/app.js
+++ 01Job app/app.js
@@ -6,7 +6,7 @@ const authRouter = require("./routes/auth");
 const jobRouter = require("./routes/job");
 
 app.use("/api/v1/auth",authRouter);
-app.use("/api/v1/job",jobRouter);
+// app.use("/api/v1/job",jobRouter);
 
 const port = process.env.PORT || 8080;
 const start=async()=>{
